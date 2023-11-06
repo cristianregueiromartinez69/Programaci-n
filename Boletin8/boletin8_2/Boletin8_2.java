3a820d1cb1dbf1c52e155c4c4c00729c34f94ad0
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2023-11-06 10:32:24 +0100

    Boletin8

---
+++ Boletin8/boletin8_2/Boletin8_2.java
@@ -0,0 +1,40 @@
+
+package boletin8_2;
+import java.util.Scanner;
+
+public class Boletin8_2 {
+
+   
+    public static void main(String[] args) {
+  
+       
+        Superficie su1=new Superficie();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Introduce una opción del 1 al 3: ");
+		su1.dameOp(sc.nextInt());
+		su1.seleecionaArea();
+
+        
+        
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+
