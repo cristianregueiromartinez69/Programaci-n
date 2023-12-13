4429ada03effc661cc802caea5362f9f4693aa40
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2023-12-13 10:40:04 +0100

    Boletin nota avaliacion final

---
+++ Boletin_Avaliacion_Final/boletin_avaliacion_final/Boletin_Avaliacion_Final.java
@@ -0,0 +1,20 @@
+
+package boletin_avaliacion_final;
+
+public class Boletin_Avaliacion_Final {
+
+ 
+    public static void main(String[] args) {
+        
+        
+        
+
+		Nota_Media n1=new Nota_Media();
+		n1.calcularNotaMedia();
+        
+        
+        
+        
+    }
+    
+}
