f4ee37f575b4cf0400d70cbd18fc405a2b1b78ba
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-01-31 10:49:15 +0100

    Boletin 24

---
+++ Boletin24/src/boletin24/Goles_Equipos.java
@@ -0,0 +1,46 @@
+
+package boletin24;
+
+
+public class Goles_Equipos {
+    
+
+    static int goles[][]=new int[20][38];
+
+	public Goles_Equipos() {
+
+
+
+	}
+
+
+
+	public static int[][] getGoles() {
+		return goles;
+	}
+
+
+
+	public static void setGoles(int[][] goles) {
+		Goles_Equipos.goles = goles;
+	}
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Boletin24/src/boletin24/Jornadas.java
@@ -0,0 +1,28 @@
+
+package boletin24;
+
+
+public class Jornadas {
+    
+    static String jornada[]= {"j1","j2","j3","j4","j5","j6","j7","j8","j9","j10","j11","j12","j13",
+			"j14","j15","j16","j17","j18","j19","j20","j21","j22","j23","j24","j25","j26","j27","j28","j29","j30",
+			"j31","j32","j33","j34","j35","j36","j37","j38"};
+
+	public Jornadas() {
+
+
+
+	}
+
+	public static String[] getJornada() {
+		return jornada;
+	}
+
+	public static void setJornada(String[] jornada) {
+		Jornadas.jornada = jornada;
+	}
+
+
+
+    
+}
